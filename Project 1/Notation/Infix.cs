847ff1f43009577c708642aa3404aff7965d427e
Ali <the.alif.dev@gmail.com>
2023-12-30 23:49:03 +0330

    Add Infix to Prefix method

--- Project 1/Notation/Infix.cs
+++ Project 1/Notation/Infix.cs
@@ -1,4 +1,6 @@
-﻿namespace Project_1.Notation
+﻿using Utils.Stack;
+
+namespace Project_1.Notation
 {
     public class Infix : BaseNotation
     {
@@ -9,6 +11,47 @@
         public override BaseNotation convert(NotationType to)
         {
             return this;
+        }
+
+        public Prefix ToPrefix()
+        {
+            var output = "";
+            var stack = new Stack<char>(value.Length);
+
+            // Check each character
+            foreach (var token in value)
+                // If char is alphanum (operand)
+                if (Utils.Utils.IsAlphaNum(token))
+                {
+                    output += token;
+                }
+                else if (token == '(')
+                {
+                    stack.Push(token);
+                }
+                else if (token == ')')
+                {
+                    char p;
+                    while (!stack.IsEmpty() && (p = stack.Pop()) != '(')
+                        output += p;
+                }
+                // Char is operator
+                else
+                {
+                    // Pops ops with higher priority from stack and add them to output
+                    while (
+                        !stack.IsEmpty() && stack.TopItem() != '(' &&
+                        GetPriority(token) >= GetPriority(stack.TopItem())
+                    )
+                        output += stack.Pop();
+
+                    stack.Push(token);
+                }
+
+            while (!stack.IsEmpty())
+                output += stack.Pop();
+
+            return new Prefix(output);
         }
     }
 }
