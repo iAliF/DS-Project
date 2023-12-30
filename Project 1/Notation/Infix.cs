0b7234599533171f910cd8c149300d9c2f0fefc3
Ali <the.alif.dev@gmail.com>
2023-12-30 23:06:37 +0330

    Add Infix/PostFix/Prefix class

---
+++ Project 1/Notation/Infix.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_1.Notation
+{
+    public class Infix : BaseNotation
+    {
+        public Infix(string value) : base(value)
+        {
+        }
+
+        public override BaseNotation convert(NotationType to)
+        {
+            return this;
+        }
+    }
+}
---
+++ Project 1/Notation/Prefix.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_1.Notation
+{
+    public class Prefix : BaseNotation
+    {
+        public Prefix(string value) : base(value)
+        {
+        }
+
+        public override BaseNotation convert(NotationType to)
+        {
+            return this;
+        }
+    }
+}
