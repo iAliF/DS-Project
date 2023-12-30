9064a59c2dd9afc297926a77f30a47a594e11e5e
Ali <the.alif.dev@gmail.com>
2023-12-30 23:02:25 +0330

    Add BaseNotation class

---
+++ Project 1/Notation/BaseNotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Project_1.Notation
+{
+    public abstract class BaseNotation
+    {
+        public Dictionary<string, int> PRIORITY = new Dictionary<string, int>
+        {
+            { "(", 0 },
+            { "!", 1 },
+            { "^", 2 },
+            { "*", 3 },
+            { "/", 3 },
+            { "%", 3 },
+            { "+", 4 },
+            { "-", 4 },
+            { "<", 5 },
+            { ">", 5 },
+            { "<=", 5 },
+            { ">=", 5 },
+            { "==", 6 },
+            { "!=", 6 },
+            { "&&", 7 },
+            { "and", 7 },
+            { "||", 8 },
+            { "or", 8 },
+        };
+
+        private readonly string _value;
+
+        public string value => value;
+
+        protected BaseNotation(string value)
+        {
+            _value = value.Replace(" ", "");
+        }
+
+        public abstract BaseNotation convert(NotationType to);
+
+        public override string ToString()
+        {
+            return $"({_value})";
+        }
+    }
+}
