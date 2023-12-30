6b381bda9dddc71f57f5b14671bc6127168930b4
Ali <the.alif.dev@gmail.com>
2023-12-30 23:48:47 +0330

    Add Utils class

---
+++ Utils/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils
+{
+    public class Utils
+    {
+        public static bool IsAlphaNum(char c)
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '1' && c <= '9');
+        }
+    }
+}
