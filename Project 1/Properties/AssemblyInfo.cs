e70331411bedaa19f077fe144bc733ce17f3dd5e
Ali <the.alif.dev@gmail.com>
2024-01-05 17:50:03 +0330

    Change namespace

--- Project 1/Properties/AssemblyInfo.cs
+++ Project 1/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DS_Project")]
+[assembly: AssemblyTitle("Project_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DS_Project")]
+[assembly: AssemblyProduct("Project_1")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
