8dfd69e27397a0451056ae5ba711e9049077b9f2
Ali <the.alif.dev@gmail.com>
2024-01-16 15:31:07 +0330

    Add LinkNode constructor

--- Project 2/LinkList/LinkNode.cs
+++ Project 2/LinkList/LinkNode.cs
@@ -5,5 +5,12 @@
         public TType Data;
         public LinkNode<TType> RLink; // Right link
         public LinkNode<TType> LLink; // Left link
+
+        public LinkNode(TType data, LinkNode<TType> rLink = null, LinkNode<TType> lLink = null)
+        {
+            Data = data;
+            RLink = rLink;
+            LLink = lLink;
+        }
     }
 }
