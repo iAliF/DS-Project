be6b3cb6a7e0137c981e9da081904c0496eb2286
Ali <the.alif.dev@gmail.com>
2024-01-16 16:48:38 +0330

    Add IsOperator method to Utils

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -6,5 +6,10 @@
         {
             return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '1' && c <= '9');
         }
+        
+        public static bool IsOperator(char c)
+        {
+            return !IsAlphaNum(c) && c != '(' && c != ')' && c != ' ';
+        }
     }
 }
