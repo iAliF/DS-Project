220aec6e79d6cffc8ffc54fa0f301afc8b7c1348
Ali <the.alif.dev@gmail.com>
2024-01-17 19:52:04 +0330

    Add BinaryTree constructors

---
+++ Project 3/Tree/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿namespace Project_3.Tree
+{
+    public class BinaryTree
+    {
+        private readonly TreeNode _root;
+
+        public TreeNode Root => _root;
+        
+        public BinaryTree()
+        {
+            _root = null;
+        }
+        
+        public BinaryTree(int data)
+        {
+            _root = new TreeNode(data);
+        }
+        
+        public BinaryTree(TreeNode root)
+        {
+            _root = root;
+        }
+    }
+}
