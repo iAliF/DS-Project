ee0b1d05ce2f544e5d4939d365ba216a3ceefd92
Ali <the.alif.dev@gmail.com>
2024-01-17 19:37:24 +0330

    Add TreeNode Class

---
+++ Project 3/Tree/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Project_3.Tree
+{
+    public class TreeNode
+    {
+        public int Data;
+        public TreeNode Parent;
+        public TreeNode LeftChild;
+        public TreeNode RightChild;
+
+        public TreeNode(int data = 0, TreeNode parent = null, TreeNode leftChild = null, TreeNode rightChild = null)
+        {
+            Data = data;
+            Parent = parent;
+            LeftChild = leftChild;
+            RightChild = rightChild;
+        }
+    }
+}
